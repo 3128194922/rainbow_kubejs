f43b8a95b518076a957aca350082642ae3783c32
3128194922 <3128194922@qq.com>
2026-06-09 11:23:44 +0800

    基因系统和代码优化

--- startup_scripts/gene/Items.js
+++ startup_scripts/gene/Items.js
@@ -1,4 +1,4 @@
-StartupEvents.registry('item', event => {
+/*StartupEvents.registry('item', event => {
     for (let i = 0; i < 14; i++) {
         event.create(`rainbow:hormone_${i}`)
             .displayName(`九龙${i}型`)
@@ -6,5 +6,5 @@ StartupEvents.registry('item', event => {
             //.color(0x2B2C7C) 
     }
 })
-
+*/
 // 【前端变前台，后端变后厨，python送到家，Java炒米粉】

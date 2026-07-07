d1c199c2517babf4ce7352ffd3453263ab5bfbc1
3128194922 <3128194922@qq.com>
2026-07-07 17:51:47 +0800

    背景图和饰品初始化

--- server_scripts/curios_initialization/main.js
+++ server_scripts/curios_initialization/main.js
@@ -1,15 +1,20 @@
 // priority: 0
 PlayerEvents.loggedIn(event => {
-    if (event.level.isClientSide()) return;
+    try{
+            if (event.level.isClientSide()) return;
     let player = event.player;
     let server = event.server;
     
     // 饰品初始化：移除旧饰品
     let Curios = ["body", "belt", "bracelet", "curio", "hands", 
         "necklace", "ring" , "rings", "feet", "hands","super_curio",
-        "legs","heart","waist","talisman",]
+        "legs","heart","waist","talisman", "informational",]
 
     Curios.forEach(curio=>{
-        server.runCommandSilent(`/curios remove ${curio} ${player.getDisplayName().getString()} 10`);
+        let result = server.runCommandSilent(`/curios remove ${curio} ${player.getDisplayName().getString()} 10`);
+        console.log("饰品初始化：移除旧饰品 " + curio + " 结果：" + result);
     })
+    }catch(e){
+        console.log("玩家饰品初始化失败：" + e);
+    }
 })
